c4cd96ac2cc265201cc59d0961aa903073afd829
Eduardo José Ferreira da Silva <101364719+DudaDevBlack@users.noreply.github.com>
2023-12-27 08:22:40 -0300

    Update script.jsdddjhtqe

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ function showProjectsMenu() {
 
     // Adicionar botão "Voltar"
     const backButton = document.createElement('button');
-    backButton.innerHTML = '<img src="https://res.cloudinary.com/dudanet/image/upload/v1703187505/voltar_seta_bidbam.png" alt="Voltar">';
+    backButton.innerHTML = '<img src="https://res.cloudinary.com/dudanet/image/upload/v1703675549/voltar_seta-removebg-preview_dspyjx.png" alt="Voltar">';
     backButton.addEventListener('click', () => {
         showProfileAndTechnologies();
     });
